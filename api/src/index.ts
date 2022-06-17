a2207aeda37ac3eac6ed04854f79127c8c2249cc
Adam Gobran <adamgobes@gmail.com>
2022-06-16 20:53:50 -0400

    wip: discord bot

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,13 @@
+import Discord from "discord.js";
 import app from "./app";
 
 const port = process.env.PORT || 3001;
+
+const client = new Discord.Client();
+client.login(process.env.DISCORD_BOT_TOKEN!);
+client.on("voiceStateUpdate", (oldState, newState) => {
+  console.log({ oldState, newState });
+});
 
 app.listen(port, () => {
   console.log("server running...");
