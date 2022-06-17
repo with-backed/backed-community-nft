891d3a66f39a86ca883e3293079163b9dfa6ecaa
Adam Gobran <adamgobes@gmail.com>
2022-06-17 10:16:08 -0400

    change check

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,8 +6,9 @@ const port = process.env.PORT || 3001;
 const client = new Discord.Client();
 client.login(process.env.DISCORD_BOT_TOKEN!);
 client.on("voiceStateUpdate", (oldState, newState) => {
+  console.log(oldState.channelID);
   // null oldState.channelID means user is joining
-  if (oldState.channelID === null) {
+  if (oldState.channelID === null || typeof oldState.channelID == "undefined") {
     console.log({ member: newState.member, channelID: newState.channelID });
   }
 });
