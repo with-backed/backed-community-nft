1cfa40580bf9da626ff367517d8984dbd0c9fed2
Adam Gobran <adamgobes@gmail.com>
2023-01-03 19:04:10 +0200

    feature: Add paprHero winner trait

---
+++ contracts/src/traits/Hero.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {IBackedBunnyAccessory} from "./IBackedBunnyAccessory.sol";
+
+contract Hero is IBackedBunnyAccessory {
+    function renderTrait() external pure override returns (string memory) {
+        return
+            string.concat(
+                string.concat(
+                    '<svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">',
+                    '<rect x="13" y="19" width="13" height="4" fill="#FF0000"/>',
+                    '<rect x="16" y="20" width="1" height="1" fill="#BB0000"/>',
+                    '<rect x="22" y="20" width="1" height="1" fill="#BB0000"/>',
+                    '<rect x="15" y="21" width="1" height="1" fill="#BB0000"/>',
+                    '<rect x="17" y="21" width="1" height="1" fill="#BB0000"/>',
+                    '<rect x="21" y="21" width="1" height="1" fill="#BB0000"/>',
+                    '<rect x="23" y="21" width="1" height="1" fill="#BB0000"/>',
+                    "</svg>"
+                )
+            );
+    }
+
+    function traitName() external pure override returns (string memory) {
+        return "Hero Visor";
+    }
+
+    function glowColor() external pure override returns (string memory) {
+        return "#aaaaaa";
+    }
+}
